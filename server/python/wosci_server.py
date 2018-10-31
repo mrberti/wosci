04efc15ca8433ed3f5dae3aa22b8985c309568d7
Simon Bertling <simon.bertling@gmx.de>
2018-10-31 23:19:53 +0900

    some refactoring on the python server

--- server/python/wosci_server.py
+++ server/python/wosci_server.py
@@ -7,41 +7,52 @@ requires autobahn
 """
 
 import json
-import asyncio
 import logging
-import time
-import random
 import numpy as np
-#import socket
+import socket
+import asyncio
 from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
 
-def get_ip(defaultIP="127.0.0.1", dns="1.1.1.1", port=80):
+logging.basicConfig(format='%(asctime)s <%(levelname)s> %(message)s', level = logging.DEBUG)
+
+def getIP(defaultIP="127.0.0.1", dns="1.1.1.1", port=80):
+    """Try to establish a connection to a DNS server and get the local IP from 
+    the socket name.
+    """
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
             s.connect((dns, port))
             ip = s.getsockname()[0]
     except Exception as e:
         ip = defaultIP
-        logging.warning("Could not get correct IP. Set to " + ip + ". " + str(e))
+        logging.warning("Could not get the correct IP. Using default: " + ip + ". " + str(e))
     return ip
 
-class MyServerProtocol(WebSocketServerProtocol):
+class WosciServerProtocol(WebSocketServerProtocol):
+    """This class implements the Autobahn websocket protocol.
+    """
 
     def __init__(self):
         super().__init__()
 
+    def sendPacket(self, data):
+        self.sendMessage(json.dumps(data).encode("UTF-8"))
+
     def onConnect(self, request):
-        logging.info("Client connecting: {0}".format(request.peer))
+        logging.info("New client connected: " + request.peer)
 
     async def onOpen(self):
         logging.info("WebSocket connection open.")
+        
+        # Create welcome message and send to client
         packet = dict()
         packet["message_type"] = "message"
         packet["message"] = "Welcome to the Autobahn server."
-        self.sendMessage(json.dumps(packet).encode("UTF-8"))
+        self.sendPacket(packet)
         await asyncio.sleep(.4)
 
         while True:
+            # Create sample data and send to client
             packet = dict()
             packet["message_type"] = "data_vectors"
             packet["data_vectors_count"] = 10
@@ -56,40 +67,38 @@ class MyServerProtocol(WebSocketServerProtocol):
                 l["values"] = data
                 data_vectors.append(l)
             packet["data_vectors"] = data_vectors
-            await asyncio.sleep(.1)
-            self.sendMessage(json.dumps(packet).encode("UTF-8"))
+            await asyncio.sleep(.05)
+            self.sendPacket(packet)
 
     def onMessage(self, payload, isBinary):
         if isBinary:
-            logging.info("Binary message received: {0} bytes".format(len(payload)))
+            logging.info("Binary message received: " + len(payload) + " bytes")
         else:
-            logging.info("Text message received: {0}".format(payload.decode('utf8')))
+            logging.info("Text message received: " + payload.decode('utf8'))
 
     def onClose(self, wasClean, code, reason):
-        logging.info("WebSocket connection closed: {0}".format(reason))
+        logging.info("WebSocket connection closed: " + reason)
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s <%(levelname)s> %(message)s', level = logging.DEBUG)
-    #logging.Formatter.converter = time.localtime
     logging.info('Starting server')
-    #server_ip = get_ip("192.168.1.80")
-    server_ip = get_ip()
+    localIP = getIP()
     port = 5678
-    serverString = u"ws://"+server_ip+":"+str(port)
+    serverString = u"ws://" + localIP + ":" + str(port)
     logging.info("Creating server on: " + serverString)
-    factory = WebSocketServerFactory(serverString)
-    factory.protocol = MyServerProtocol
 
-    loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '0.0.0.0', 5678)
-    server = loop.run_until_complete(coro)
+    factory = WebSocketServerFactory(serverString)
+    factory.protocol = WosciServerProtocol
+
+    asyncioLoop = asyncio.get_event_loop()
+    coRoutine = asyncioLoop.create_server(factory, '0.0.0.0', port)
+    server = asyncioLoop.run_until_complete(coRoutine)
 
     try:
-        loop.run_forever()
+        asyncioLoop.run_forever()
     except KeyboardInterrupt:
         pass
     finally:
         logging.info("Closing server...")
         server.close()
-        loop.close()
+        asyncioLoop.close()
         logging.info("Bye bye!")
